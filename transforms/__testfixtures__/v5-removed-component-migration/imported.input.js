828e6acd0ba578a27941aed7eed137e2d2ca626e
vagusx <vagusxl@gmail.com>
2022-11-27 15:49:07 +0800

    feat: support FloatButton.BackTop

---
+++ transforms/__testfixtures__/v5-removed-component-migration/imported.input.js
@@ -0,0 +1,9 @@
+import { BackTop, FloatButton as FButton } from 'antd';
+import React from 'react';
+const App = () => {
+  return (
+    <BackTop />
+  );
+};
+
+export default App;
---
+++ transforms/__testfixtures__/v5-removed-component-migration/imported.output.js
@@ -0,0 +1,7 @@
+import { FloatButton as FButton } from 'antd';
+import React from 'react';
+const App = () => {
+  return (<FButton.BackTop />);
+};
+
+export default App;
