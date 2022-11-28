b90954655d9d54c7016281838caeacbdd0b98abd
vagusx <vagusxl@gmail.com>
2022-11-28 17:43:40 +0800

    fix: update

--- less-transforms/postcss-plugin-remove-antd-less.js
+++ less-transforms/postcss-plugin-remove-antd-less.js
@@ -5,15 +5,13 @@ module.exports = (opts = {}) => {
     postcssPlugin: 'postcss-plugin-remove-antd-less',
     AtRule: {
       import(atRule, { postcss }) {
-        // handle forked antd
-        if (atRule.params.includes('antd')) {
-          const params = atRule.params.replace(atRule.options || '', '');
-          if (testAntdImportRegex.test(params)) {
-            const commentedRule = postcss.comment({
-              text: `${atRule.toString()};`,
-            });
-            atRule.replaceWith(commentedRule);
-          }
+        // TODO: handle forked antd
+        const params = atRule.params.replace(atRule.options || '', '');
+        if (testAntdImportRegex.test(params)) {
+          const commentedRule = postcss.comment({
+            text: `${atRule.toString()};`,
+          });
+          atRule.replaceWith(commentedRule);
         }
       },
     },
