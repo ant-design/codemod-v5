dc8a5bfa38e91079e824c76766fdc10f8198962d
vagusX <vagusX@users.noreply.github.com>
2022-11-28 17:45:06 +0800

    feat: remove compatible css import (#9)

--- less-transforms/postcss-plugin-remove-antd-less.js
+++ less-transforms/postcss-plugin-remove-antd-less.js
@@ -1,19 +1,17 @@
-const testAntdImportRegex = /(?:\(.+\)\s+)?(?:"|').+(~?antd\/.+\.less)(?:"|');?$/;
+const testAntdImportRegex = /(?:\(.+\)\s+)?(?:"|').*(~?antd\/.+\.less|~?@ant-design\/compatible\/assets\/index\.css)(?:"|');?$/;
 
 module.exports = (opts = {}) => {
   return {
     postcssPlugin: 'postcss-plugin-remove-antd-less',
     AtRule: {
       import(atRule, { postcss }) {
-        // handle forked antd
-        if (atRule.params.includes('antd')) {
-          const params = atRule.params.replace(atRule.options || '', '');
-          if (testAntdImportRegex.test(params)) {
-            const commentedRule = postcss.comment({
-              text: `${atRule.toString()};`,
-            });
-            atRule.replaceWith(commentedRule);
-          }
+        // TODO: handle forked antd
+        const params = atRule.params.replace(atRule.options || '', '');
+        if (testAntdImportRegex.test(params)) {
+          const commentedRule = postcss.comment({
+            text: `${atRule.toString()};`,
+          });
+          atRule.replaceWith(commentedRule);
         }
       },
     },
--- transforms/__testfixtures__/v5-remove-style-import/basic.input.js
+++ transforms/__testfixtures__/v5-remove-style-import/basic.input.js
@@ -1,5 +1,6 @@
 import 'antd/es/auto-complete/style';
 import 'antd/lib/button/style/index.less';
 import 'antd/dist/antd.compact.min.css';
+import '@ant-design/compatible/assets/index.css';
 
 const a = 1;
